2e5e646dbf8679ca7bd7109bdbf8844a7df474fd
hasan <hasanelmi678@gmail.com>
2022-12-16 11:50:41 +0000

    completed ICO app

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -84,6 +84,7 @@ export default function Home() {
     try {
       const signer = getProviderOrSigner(true); // Get the signer from web3Modal, needed to write
       const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, signer); // Create an instance of the token contract
+      console.log("HERE")
       const value = 0.001 * amount; // Each token is of 0.001 ether. The value we need to send is 0.001 * amount
       const tx = await tokenContract.mint(amount, { value: utils.parseEther(value.toString()) }); // Value signifies the cost of one crypto dev token which is "0.001" eth.
       setLoading(true); // Starts the loading when transaction is being mined
@@ -94,6 +95,26 @@ export default function Home() {
       await getTotalTokensMinted(); // Retrieves how many tokens have been minted till now * out of the total supply
       await getTokensToBeClaimed(); // Checks the balance of tokens that can be claimed by the user
 
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * claimCryptoDevTokens: Help the user claim Crypto Dev Tokens
+   */
+  const claimCryptoDevTokens = async () => {
+    try {
+      const signer = getProviderOrSigner(true);
+      const tokenContract = new Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_CONTRACT_ABI, signer);
+      const tx = await tokenContract.claim();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert("Successfully claimed Crypto Dev Tokens.");
+      await getBalanceOfCryptoDevTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
     } catch (error) {
       console.error(error);
     }
@@ -160,12 +181,12 @@ export default function Home() {
     const web3Provider = new providers.Web3Provider(provider);
 
     const { chainId } = await web3Provider.getNetwork();
-    if(chainId !== 5) {
+    if (chainId !== 5) {
       window.alert("Change the network to Georli.");
       throw new Error("Change the network to Georli.");
     }
 
-    if(needSigner) {
+    if (needSigner) {
       const signer = web3Provider.getSigner();
       return signer;
     }
@@ -185,7 +206,7 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if(!walletConnected) {
+    if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
         network: "goerli",
         providerOptions: {},
@@ -198,10 +219,90 @@ export default function Home() {
       getTokensToBeClaimed();
       getOwner();
     }
-  }, [walletConnected])
+  }, [walletConnected]);
+
+  /**
+   * renderButton: Returns a button based on the state of the dApp
+   */
+  const renderButton = () => {
+    if (loading) {
+      return (
+        <div>
+          <button className={styles.button}>Loading...</button>
+        </div>
+      )
+    }
+
+    // If tokens to be claimed are greater than 0, Return a claim button
+    if (tokensToBeClaimed > 0) {
+      return (
+        <div>
+          <div className={styles.description}>
+            {tokensToBeClaimed * 10} Tokens to be claimed
+          </div>
+          <button className={styles.button} onClick={claimCryptoDevTokens}>
+            Claim Tokens
+          </button>
+        </div>
+      )
+    }
+
+    // IF user doesn't have any tokens to claim, show the mint button
+    return (
+      <div style={{ display: 'flex-col' }}>
+        <div>
+          <input type="number" placeholder="Amount of tokens"
+            onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
+            className={styles.input} />
+        </div>
+        <button className={styles.button} disabled={!(tokenAmount > 0)} onClick={() => mintCryptoDevToken(tokenAmount)}>
+          Mint Tokens
+        </button>
+      </div>
+    )
+  }
 
 
   return (
-    <></>
+    <div>
+      <Head>
+        <title>Crypto Devs</title>
+        <meta name="description" content="ICO-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs ICO!</h1>
+          <div className={styles.description}>
+            You can claim or mint Crypto Dev Tokens here
+          </div>
+          {walletConnected ? (
+            <div>
+              <div className={styles.description}>
+                You have minted {utils.formatEther(balanceOfCryptoDevTokens)} Crypto Dev Tokens
+              </div>
+              <div className={styles.description}>
+                Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!
+              </div>
+              {renderButton()}
+              {isOwner ? (
+                <div>
+                  {loading ? <button className={styles.button}>Loading...</button> :
+                  <button className={styles.button} onClick={withdrawCoins}>Withdraw coins</button>}
+                </div>
+              ) : ("")}
+            </div>
+          ) : (
+            <button onClick={connectWallet} className={styles.button}>Connect your wallet</button>
+          )}
+        </div>
+        <div>
+          <img className={styles.image} src="./0.svg" />
+        </div>
+      </div>
+      <footer className={styles.footer}>
+        Made with by &#10084; Crypto Devs
+      </footer>
+    </div>
   )
 }
